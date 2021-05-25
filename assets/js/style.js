ad3db555c93b6bf3f91fc6d06b9d8b769841104f
odupree1988 <odupree1988@gmail.com>
2021-05-24 23:17:02 -0400

    added on click event for task div

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,14 +1,30 @@
-//add current date dynamically to header
+// on page load
+// check local storage to see if anything in there
+// if local storage exists
+//   update dom with values
+//   update local javascript object
+// else if nothing in storage
+//   create local storage object
+//   save to local storage
 
-//add 5am - 5pm time blocks
+// on description box click
+// check local object (created form local storage)
+// if time slot has data
+//   create input element
+//   set value of new input to the value from local storage
+//   inject html (your new input) into the description
+// else if no value in local storage
+//   create input element
+//   inject hmtl into description
 
-//color code each time block to past(grey), present(red), future(green)
+// On save click
+// target input field in parent sibiling
+// get value of input
+// save value to local object
+// save local object to local storage
+// remove input from description
+// add text to description with the data in local storage
 
-//dynamically add events to the time block and save with a click.event
-
-//saves to local storage
-
-//upon refresh events stay on page
 var m = moment();
 var currentTimeEl = m.format("dddd, MMMM Do, hA");
 var $currentDayEl = $("#currentDay");
@@ -16,9 +32,17 @@ var $descriptionEl = $(".description");
 
 $currentDayEl.html(currentTimeEl);
 
-$descriptionEl.on("click", "span", function () {
-  var text = $(this).text().trim();
-  var textInput = $("<textarea>").addClass("form-control").val(text);
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
+$(function () {
+  $(".saveBtn").on("click", function(e) {
+    var $this = e
+    var taskText = $($this.target).parent()
+    console.log(taskText)
+
+  })
+  $(".description").on("click", function (e) {
+    var $this = e;
+    var inputEl = $("<input type='text'/>");
+    $($this.target).html(inputEl);
+  });
 });
+
