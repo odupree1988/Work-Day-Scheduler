183a7c02d5af39532d589593a9febb6154c83fe1
odupree1988 <odupree1988@gmail.com>
2021-05-30 00:52:49 -0400

    removed pseudocode

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,30 +1,3 @@
-// on page load
-// check local storage to see if anything in there
-// if local storage exists
-//   update dom with values
-//   update local javascript object
-// else if nothing in storage
-//   create local storage object
-//   save to local storage
-
-// on description box click
-// check local object (created form local storage)
-// if time slot has data
-//   create input element
-//   set value of new input to the value from local storage
-//   inject html (your new input) into the description
-// else if no value in local storage
-//   create input element
-//   inject hmtl into description
-
-// On save click
-// target input field in parent sibiling
-// get value of input
-// save value to local object
-// save local object to local storage
-// remove input from description
-// add text to description with the data in local storage
-
 var m = moment();
 var currentTimeEl = m.format("dddd, MMMM Do, hA");
 var $currentDayEl = $("#currentDay");
